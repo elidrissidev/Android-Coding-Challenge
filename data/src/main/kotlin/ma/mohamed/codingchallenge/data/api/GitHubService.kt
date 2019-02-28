181ef3ac141401f0e1a1bf97de9ad480cd338838
Mohamed ELIDRISSI <mohamed@Mohamed-PC>
2019-02-28 23:46:15 +0100

    Added GitHubService

---
+++ data/src/main/kotlin/ma/mohamed/codingchallenge/data/api/GitHubService.kt
@@ -0,0 +1,20 @@
+package ma.mohamed.codingchallenge.data.api
+
+import io.reactivex.Single
+import ma.mohamed.codingchallenge.data.model.PagedResponseModel
+import ma.mohamed.codingchallenge.data.model.RepoModel
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GitHubService {
+
+    @Headers("Accept: application/json")
+    @GET("search/repositories?q=created:>{fromDate}")
+    fun getRepos(
+        @Path("fromDate") fromDate: String,
+        @Query("sort") sortBy: String = "stars",
+        @Query("order") order: String = "desc"
+    ): Single<PagedResponseModel<RepoModel>>
+}
