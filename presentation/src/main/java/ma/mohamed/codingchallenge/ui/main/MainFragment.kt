3e676af4c516fa64ab578dcb56d06e04ffb1faa4
Mohamed ELIDRISSI <mohamed@Mohamed-PC>
2019-03-01 10:20:27 +0100

    Added MainFragment and navigation graph

---
+++ presentation/src/main/java/ma/mohamed/codingchallenge/ui/main/MainFragment.kt
@@ -0,0 +1,29 @@
+package ma.mohamed.codingchallenge.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dagger.android.support.DaggerFragment
+import ma.mohamed.codingchallenge.R
+
+class MainFragment : DaggerFragment() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        recyclerView = view.findViewById(R.id.recycler_view)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)
+        }
+    }
+}
